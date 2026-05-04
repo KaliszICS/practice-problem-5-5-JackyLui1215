3733c0472947553f257374a528814c96ca367166
JackyLui1215 <343344461@tdsb.ca>
2026-05-04 15:33:49 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,17 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean isPalindrome(String word) {
+		word = word.replace(" ", "");
+		String[] isPalindrome = word.split("");
+		for (int i = 0; i < isPalindrome.length; i++) {
+			if (!isPalindrome[i].equalsIgnoreCase(isPalindrome[isPalindrome.length - 1 - i])) {
+			return false;
+		}
+		}
+		return true;
+	}
 }
+	
+
+
